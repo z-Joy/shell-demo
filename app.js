057ccf0026086564327484838717da164120ff89
zhengjinhan <jinhan.zheng@inmeijia.com>
2021-10-08 10:53:39 +0800

    feat: init

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
-const { execFile } = require('child_process')
+const { execFile } = require('child_process');
+const cwd = process.cwd();
 
 const hostname = '127.0.0.1';
 const port = 3333;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
   if (req.url === '/install') {
-    execFile('./install.sh', (err, studot) => {
-      console.log(studot);
+    console.log(111);
+    execFile(cwd + './install.sh', (err, studot) => {
+      console.log(err, studot);
     })
   }
   res.end('hello, world');
